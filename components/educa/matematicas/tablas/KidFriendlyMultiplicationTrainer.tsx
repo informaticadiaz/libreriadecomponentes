9f8f9ab13f6d7376407b7ff5c8c88a974478c25d
ignacio <informatica.diaz@gmail.com>
2025-09-20 14:49:10 -0300

    .

--- components/educa/matematicas/tablas/KidFriendlyMultiplicationTrainer.tsx
+++ components/educa/matematicas/tablas/KidFriendlyMultiplicationTrainer.tsx
@@ -1,4 +1,4 @@
-"use lient"
+"use client"
 import React, { useState, useRef, useEffect } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Button } from '@/components/ui/button';
